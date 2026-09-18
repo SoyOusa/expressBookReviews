4c72936b38b6dfb714d270fb2a6eaca4c3d4e97e
SoyOusa <soyousa31@gmail.com>
2026-09-18 12:27:45 -0400

    task 7-8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,16 +13,74 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
+//Task 7:
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  if (!username || !password) {
+    return res.status(400).json({
+        message: "username and password are required"
+    });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+        {username: username}, 
+        "access", 
+        {expiresIn: 60 * 60}
+    );
+
+    req.session.authorization = {
+        accessToken: accessToken,
+        username: username
+    };
+
+    return res.status(200).json(
+        {message: "User successfully logged in",
+        accessToken: accessToken
+    });
+  } else {
+    return res.status(401).json({
+        message: "Invalid Login. Check username and password"
+    });
+  }
+  
 });
 
+//Task 8  
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn; 
+  const review = req.query.reviewl
+
+  const username = req.session.authorization.username;
+
+  if(!books[isbn]) {
+    return res.status(404).json({
+        message: "Book not found"
+    });
+  }
+
+  if (!review) {
+    return res.status(400).json({
+        message: "review is required"
+    });
+  }
+  if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review successfully added/updated",
+    reviews: books[isbn].reviews\
+  });
 });
 
 module.exports.authenticated = regd_users;
